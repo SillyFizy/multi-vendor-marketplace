1162ac42a3475f662adcfa4f2bb984367cfdabb5
SillyFizy <misterchatty@hotmail.com>
2024-01-10 11:07:33 +0300

    first commit

---
+++ Frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <header
+    class="flex justify-center bg-[#FFFCF2] p-4 w-full mb-8 h-[80px] shadow-l sticky top-0 z-50 transition-all ease-out duration-1000">
+    <div class="w-5/6 flex justify-between items-center">
+      <div class="flex justify-center items-center">
+        <img class="w-[10%]" src="./assets/logo.svg" alt="">
+        <h1>PC HUB</h1>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  </header>
+</template>
+<script setup>
+</script>
