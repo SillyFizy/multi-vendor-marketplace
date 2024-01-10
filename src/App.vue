d53f6379f96fd72503bf5a713d518fd287c4378f
jap610 <hassn.ahmed610@yahoo.com>
2024-01-10 21:39:01 +0300

    second

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,21 @@
   <header
     class="flex justify-center bg-[#FFFCF2] p-4 w-full mb-8 h-[80px] shadow-l sticky top-0 z-50 transition-all ease-out duration-1000 h-1/12">
     <div class="h-full w-5/6 flex justify-between items-center ">
-      <div class="flex justify-start items-center w-1/3 ">
-        <img class="w-[12%] mr-5"  src="./assets/logo.svg" alt="">
+      <div class="flex justify-between items-center w-1/2 ">
+        <img class="w-[10%] "  src="./assets/logo.svg" alt="">
         
-        <h1 id="siteName" class="text-[50px]"><span class="text-[#EB5E28] text-[100%]">PC</span>HUB</h1>
+        <h1 id="siteName" class="text-[50px] "><span class="text-[#EB5E28] text-[100%]">PC</span>HUB</h1>
+        <div class="w-1/2 justify-start flex border-l-[#d9d9d9] border-l-[40px] rounded-lg relative ">
+          <img src="./assets/search-md.svg " class="absolute left-[-35px] top-[2px]"  alt="">
+          <input class="w-1/2 bg-[#d9d9d9] rounded-r-lg h-8 outline-[#EB5E28]  "  placeholder=" Search" type="text">
+        </div>
         <!--the id is used to implement font but it didint work for some reason go see https://fontsource.org/fonts/orbitron/install
         cheak the main.js faile and the style.css file.-->
       </div>
-      <div class="w-1/3 justify-start flex  "><input class="w-1/2 bg-[#d9d9d9] rounded-lg h-8 "  placeholder=" Search" type="text"></div>
+      
       <div class=" w-1/3 flex justify-end items-center">
-        <div class="mr-5 bg-[#EB5E28] w-1/5 h-8 justify-center flex items-center rounded-lg z-20 font-semibold"  >login</div>
-        <div class="mr-5  w-1/5 h-8 justify-center flex items-center rounded-lg font-semibold z-20 bg-[#d9d9d9]">signup</div>
+        <Button class="bg-[#EB5E28] hover:bg-[#B64518]">Login</Button>
+        <Button class="ml-4 mr-4 bg-[#d4d4d4] hover:bg-[#a09f9f]">SignUp</Button>
         <div>
           <img class= "w-12 mr-5" src="./assets/user-circle.svg" alt="">
         </div>
@@ -24,4 +28,5 @@
   </header>
 </template>
 <script setup>
+import Button from './components/Button.vue';
 </script>
---
+++ src/components/Button.vue
@@ -0,0 +1,9 @@
+<template>
+<button class="text-black  w-1/5 h-8 justify-center flex rounded-lg z-20 font-semibold transition-all px-5 py-2 items-center ">
+    <slot></slot>
+</button>
+</template>
+
+<script setup>
+
+</script>
